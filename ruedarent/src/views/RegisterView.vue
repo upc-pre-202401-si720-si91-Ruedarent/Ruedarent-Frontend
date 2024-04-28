3cb88a13c44bac467788f60fe52ff49e37804e4b
Alejandro Rodriguez <alejandro.lr.developer@hotmail.com>
2024-04-28 14:14:01 -0500

    feat: added register form

---
+++ ruedarent/src/views/RegisterView.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="register">
+    <form class="form">
+      <div class="form__group">
+        <label for="name">Name</label>
+        <input id="name" type="text" placeholder="John Doe" />
+      </div>
+      <div class="form__group">
+        <label for="email">Email</label>
+        <input id="email" type="email" placeholder="john@email.com" />
+      </div>
+      <div class="form__group">
+        <label for="password">Password</label>
+        <input type="password" placeholder="*********" />
+      </div>
+      <button type="submit">Register</button>
+    </form>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .register {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+  .form {
+    display: flex;
+    flex-direction: column;
+    gap: 0.2rem;
+    padding: 1rem;
+    width: 420px;
+  }
+
+  .form__group {
+    display: flex;
+    gap: .2rem;
+    flex-direction: column;
+  }
+
+  input {
+    padding: 0.5rem;
+    border-radius: 5px;
+    border: none;
+  } 
+
+  button {
+    padding: .5rem;
+    border: none;
+    margin-top: .5rem;
+    border-radius: 5px;
+    cursor: pointer;
+  }
+
+  button:hover {
+    background-color: #eeee;
+  }
+}
+</style>
