c674a5b0a09da1d0e77fce06a60f18f5e94e47d1
Juan Calisaya <juancalijesus@gmail.com>
2024-06-04 22:21:54 -0500

    fix: correct form display after merge

--- src/views/Auth/LoginView.vue
+++ src/views/Auth/LoginView.vue
@@ -3,10 +3,10 @@
     <div class="flex flex-col justify-center items-center gap-2">
       <img class="w-20 h-20" src="/src/assets/logoo.jpeg" alt="logo" />
       <h1 class="text-2xl font-medium">Inicia sesión</h1>
-      <ul class="flex flex-col items-center">
+      <ul class="flex items-start gap-2">
         <li>
-          <button>
-            <img class="social-logo3"
+          <button class="flex flex-row items-center gap-2">
+            <img class="social-logo2"
                  src="https://i.ibb.co/PgjqCyd/image.png"
                  alt="Google Logo"/>
             Iniciar sesión con Google
@@ -15,9 +15,9 @@
         <li>
           <button class="flex flex-row items-center gap-2">
             <img
-              class="social-logo2"
-              src="https://i.ibb.co/H4Zc1QR/image.png"
-              alt="Microsoft Logo"
+                class="social-logo2"
+                src="https://i.ibb.co/H4Zc1QR/image.png"
+                alt="Microsoft Logo"
             />
             Iniciar sesión con Microsoft
           </button>
@@ -27,73 +27,61 @@
     <form class="form">
       <div class="form__group">
         <label for="email">Correo electrónico</label>
-        <input id="email" type="email" placeholder="Correo electrónico"/>
+        <input id="email" type="email" placeholder="Correo electrónico" />
       </div>
       <div class="form__group">
         <label for="contraseña">Contraseña</label>
-        <input type="contraseña" placeholder="Contraseña"/>
+        <input type="contraseña" placeholder="Contraseña" />
       </div>
       <button type="submit">Iniciar sesión</button>
     </form>
-   <span>
-    ¿No tienes una cuenta? <a href="/register">Regístrate</a>
-   </span>
+    <span> ¿No tienes una cuenta? <a href="/register">Regístrate</a> </span>
   </div>
 </template>
 
 <style>
-  .login__socials div {
-    display: flex;
-    flex-direction: column;
-  }
+.form {
+  display: flex;
+  flex-direction: column;
+  gap: 0.2rem;
+  padding: 1rem;
+  width: 420px;
+}
 
-  .form {
-    display: flex;
-    flex-direction: column;
-    gap: 0.2rem;
-    padding: 1rem;
-    width: 420px;
-  }
+.form__group {
+  display: flex;
+  gap: 0.2rem;
+  flex-direction: column;
+}
 
-  .form__group {
-    display: flex;
-    gap: .2rem;
-    flex-direction: column;
-  }
+input {
+  padding: 0.5rem;
+  border-radius: 5px;
+  border: none;
+  border: 1px solid #333;
+}
 
-  input {
-    padding: 0.5rem;
-    border-radius: 5px;
-    border: none;
-    border: 1px solid #333;
-  } 
+button {
+  padding: 0.5rem;
+  border: none;
+  margin-top: 0.5rem;
+  border-radius: 5px;
+  cursor: pointer;
+  background-color: #34d399;
+  color: white;
+}
 
-  button {
-    padding: .5rem;
-    border: none;
-    margin-top: .5rem;
-    border-radius: 5px;
-    cursor: pointer;
-    background-color: #34D399;
-    color: white;
-  }
+button:hover {
+  background-color: #53a37b;
+}
 
-  button:hover {
-    background-color: #53A37B;
-  }
-
-  span a {
-    color: #34D399
-  }
+span a {
+  color: #34d399;
+}
 
 .social-logo2 {
   width: 17px;
   text-align: center;
   vertical-align: middle;
 }
- .social-logo3 {
-   width: 21px;
-   float: left;
-   vertical-align: middle;
- }
 </style>
--- src/views/Auth/RegisterView.vue
+++ src/views/Auth/RegisterView.vue
@@ -3,11 +3,11 @@
     <div class="flex flex-col justify-center gap-2 items-center">
       <img class="w-20 h-20" src="/src/assets/logoo.jpeg" alt="logo" />
       <h1 class="text-2xl font-medium">Regístrate</h1>
-      <ul class="flex flex-col items-center">
+      <ul class="flex items-start gap-2">
         <li>
-          <button>
-            <img class="social-logo3"
-                 src="https://i.ibb.co/PgjqCyd/image.png"
+          <button class="flex flex-row items-center gap-2">
+            <img class="social-logo2"
+                src="https://i.ibb.co/PgjqCyd/image.png"
                  alt="Google Logo"/>
             Regístrate con Google
           </button>
@@ -15,141 +15,102 @@
         <li>
           <button class="flex flex-row items-center gap-2">
             <img
-              class="social-logo2"
-              src="https://i.ibb.co/H4Zc1QR/image.png"
-              alt="Microsoft Logo"
+                class="social-logo2"
+                src="https://i.ibb.co/H4Zc1QR/image.png"
+                alt="Microsoft Logo"
             />
             Regístrate con Microsoft
           </button>
         </li>
       </ul>
     </div>
-    <form class="form">
-      <div class="form__group">
-        <div class="form__group__top">
-          <input id="name" type="text" placeholder="Nombres"/>
-          <input id="lastName" type="text" placeholder="Apellidos"/>
+    <form class="flex flex-col justify-center items-center gap-2 p-1">
+      <div class="flex gap-1 flex-col w-full">
+        <div class="flex flex-row justify-between gap-1">
+          <input id="name" type="text" placeholder="Nombres" />
+          <input id="lastName" type="text" placeholder="Apellidos" />
         </div>
-        <div class="form__group__bot">
-          <input id="email" type="email" placeholder="Correo electrónico"/>
+        <div class="flex flex-col gap-1">
+          <input id="email" type="email" placeholder="Correo electrónico" />
         </div>
       </div>
-      <div class="form__group">
-        <div class="form__group__top">
-          <input id="password" type="password" placeholder="Contraseña"/>
-          <input id="passwordConfirm" type="password" placeholder="Confirmación"/>
+      <div class="flex gap-1 flex-col w-full">
+        <div class="flex flex-row justify-around gap-1">
+          <input id="password" type="password" placeholder="Contraseña" />
+          <input
+              id="passwordConfirm"
+              type="password"
+              placeholder="Confirmación"
+          />
         </div>
-        <div class="form__group__bot">
-          <input id="dob" type="date" placeholder="Fecha de nacimiento"/>
+        <div class="flex flex-col gap-1">
+          <input id="dob" type="date" placeholder="Fecha de nacimiento" />
           <label>DD/MM/AAAA</label>
         </div>
       </div>
-      <div class="form__group">
-        <div class="form__group__top">
-         <div class="gender-select">
-          <input type="radio" id="masculino"/>
-          <label for="masculino" id="masculino">Masculino</label>
-         </div>
-         <div class="gender-select">
-          <input type="radio" id="femenino" />
-          <label for="female" id="femenino">Femenino</label>
-         </div>
+      <div class="flex gap-1 flex-col w-full">
+        <div class="flex flex-row justify-between">
+          <div class="gender-select">
+            <input type="radio" id="masculino" />
+            <label for="masculino" id="masculino">Masculino</label>
+          </div>
+          <div class="gender-select">
+            <input type="radio" id="femenino" />
+            <label for="female" id="femenino">Femenino</label>
+          </div>
         </div>
       </div>
       <button type="submit">Crear cuenta</button>
     </form>
-   <span>
-    ¿Tienes una cuenta? <a href="/login">Inicia sesión</a>
-   </span>
+    <span> ¿Tienes una cuenta? <a href="/login">Inicia sesión</a> </span>
   </div>
 </template>
 
 <style>
-  .login__socials div {
-    display: flex;
-    flex-direction: column;
-  }
+input {
+  padding: 0.5rem;
+  border-radius: 5px;
+  border: none;
+  border: 1px solid #333;
+}
 
-  .form {
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    gap: 0.8rem;
-    padding: 1rem;
-    width: 420px;
-  }
+button {
+  padding: 0.5rem;
+  border: none;
+  margin-top: 0.5rem;
+  border-radius: 5px;
+  cursor: pointer;
+  background-color: #34d399;
+  color: white;
+}
 
-  .form__group {
-    display: flex;
-    gap: .8rem;
-    width: 100%;
-    flex-direction: column;
-  }
+.form__group__bot label {
+  font-size: small;
+  text-decoration: underline;
+}
 
-  .form__group__top {
-    display: flex;
-    flex-direction: row;
-    justify-content: space-between;
-  }
+button:hover {
+  background-color: #53a37b;
+}
 
-  .form__group__top:last-child {
-    display: flex;
-    flex-direction: row;
-    justify-content: space-around;
-  }
-
-  .form__group__bot {
-    display: flex;
-    gap: .8rem;
-    flex-direction: column;
-  }
-
-  input {
-    padding: 0.5rem;
-    border-radius: 5px;
-    border: none;
-    border: 1px solid #333;
-  } 
-
-  button {
-    padding: .5rem;
-    border: none;
-    margin-top: .5rem;
-    border-radius: 5px;
-    cursor: pointer;
-    background-color: #34D399;
-    color: white;
-  }
-
-  .form__group__bot label {
-    font-size: small;
-    text-decoration: underline;
-  }
-
-  button:hover {
-    background-color: #53A37B;
-  }
-
-  span a {
-    color: #34D399
-  }
+span a {
+  color: #34d399;
+}
 
 .social-logo2 {
   width: 17px;
   text-align: center;
   vertical-align: middle;
 }
-  .social-logo2 {
-    width: 17px;
-    text-align: center;
-    vertical-align: middle;
-  }
 
-  .social-logo3 {
-    width: 21px;
-    float: left;
-    vertical-align: middle;
-  }
+.social-logo3 {
+  width: 17px;
+  margin-right: 0.5rem;
+}
 
+img {
+  width: 17px;
+}
 </style>
+<script setup lang="ts">
+</script>
