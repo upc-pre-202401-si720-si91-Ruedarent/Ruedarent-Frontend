1eb25c3e57ae2bcc8ff3a83e9d1ef8ebbf0cc99a
Juan Calisaya <juancalijesus@gmail.com>
2024-06-08 13:24:39 -0500

    feat: added api fake ruedarent

--- src/shared/services/http-common.js
+++ src/shared/services/http-common.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:3000/';
+// Cambia esta URL por la URL de tu API en Mocky.io
+const API_BASE_URL = 'https://run.mocky.io/v3/be8f67e4-5590-40d7-87a8-2ff367d4d5a3';
 
 const http = axios.create({
   baseURL: API_BASE_URL,
@@ -9,12 +10,12 @@ const http = axios.create({
 
 export async function obtenerVehiculos() {
   try {
-    const response = await http.get('vehiculos');
-     if (response.status !== 200) {
+    const response = await http.get('/');
+    if (response.status !== 200) {
       throw new Error('No se pudo obtener la información de los vehículos');
     }
     const data = response.data;
-    return data;
+    return data.vehiculos; // Ajusta esto según la estructura de tu JSON en Mocky.io
   } catch (error) {
     console.error('Error al obtener datos de vehículos:', error);
     return null;
@@ -22,17 +23,17 @@ export async function obtenerVehiculos() {
 }
 
 export async function obtenerUsuarios() {
-    try {
-      const response = await http.get('usuarios');
-       if (response.status !== 200) {
-        throw new Error('No se pudo obtener la información de los vehículos');
-      }
-      const data = response.data;
-      return data;
-    } catch (error) {
-      console.error('Error al obtener datos de vehículos:', error);
-      return null;
+  try {
+    const response = await http.get('/');
+    if (response.status !== 200) {
+      throw new Error('No se pudo obtener la información de los usuarios');
     }
+    const data = response.data;
+    return data.usuarios; // Ajusta esto según la estructura de tu JSON en Mocky.io
+  } catch (error) {
+    console.error('Error al obtener datos de usuarios:', error);
+    return null;
+  }
 }
 
 export default http;
